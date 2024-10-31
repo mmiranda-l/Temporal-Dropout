b066467ae477305b981068da8b7691aa70b916e0
Francisco Mena <famenatoro@gmail.com>
2024-10-31 12:13:40 +0100

    update transformer

--- src/models/single/encoders.py
+++ src/models/single/encoders.py
@@ -1,5 +1,5 @@
 import torch
-from torch import nn
+from torch import nn, Tensor
 
 from .base_encoders import Base_Encoder
 
@@ -101,6 +101,7 @@ class TransformerNet(Base_Encoder):
         num_heads: int = 1,
         len_max_seq: int = 24,
         fixed_pos_encoding: bool = True,
+        pre_embedding=True,
         **kwargs,
     ):
         super(TransformerNet, self).__init__()
@@ -112,23 +113,27 @@ class TransformerNet(Base_Encoder):
         self.num_heads = num_heads
         self.fixed_pos_encoding = fixed_pos_encoding
 
-        self.cls_token = torch.nn.Parameter(torch.zeros(1, 1, self.feature_size))  
+        self.embedding_layer = EmbeddingLayer(in_features=self.feature_size, out_features=self.layer_size, model_type="linear") if pre_embedding else nn.Identity()
+        self.emb_dim = self.layer_size if pre_embedding else self.feature_size
+
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, self.emb_dim))  
         self.pos_encoder = PositionalEncoding(
-            d_model=self.feature_size, 
+            d_model=self.emb_dim, 
             dropout=self.dropout,
             max_len=self.len_max_seq)
         
-        encoder_layer = torch.nn.TransformerEncoderLayer(
-            d_model=self.feature_size,
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model=self.emb_dim,
             nhead=self.num_heads,
-            dim_feedforward=self.layer_size,
+            #dim_feedforward=self.layer_size,
             dropout=self.dropout,
             batch_first=True,
             **kwargs
         )
-        self.tr_encoder = torch.nn.TransformerEncoder(encoder_layer=encoder_layer,num_layers=self.num_layers)
+        self.tr_encoder = nn.TransformerEncoder(encoder_layer=encoder_layer,num_layers=self.num_layers)
 
     def forward(self, x, src_key_padding_mask=None, **kwargs):
+        x = self.embedding_layer(x)
         # add cls token to input sequence
         x = torch.cat((self.cls_token.expand(x.shape[0], -1, -1), x), 1)
 
@@ -149,10 +154,14 @@ class TransformerNet(Base_Encoder):
         return {"rep": x[:, 0, :]} # extract class token feature: 
 
     def get_output_size(self):
-        return self.feature_size
-
+        return self.emb_dim
 
 class PositionalEncoding(nn.Module):
+    """
+    ref: https://d2l.ai/chapter_attention-mechanisms-and-transformers/self-attention-and-positional-encoding.html#positional-encoding
+
+    """
+
     def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
         super().__init__()
 
@@ -164,15 +173,16 @@ class PositionalEncoding(nn.Module):
         position = torch.arange(
             max_len, dtype=torch.float32
         ).reshape(-1, 1) / torch.pow(torch.tensor(10000), torch.arange(0, d_model, 2, dtype=torch.float32) / d_model)
+
         position_encoding[0, :, 0::2] = torch.sin(position)
-        position_encoding[0, :, 1::2] = torch.cos(position[:, :(-1 if d_model%2 != 0 else int(d_model/2))])
+        position_encoding[0, :, 1::2] = torch.cos(position)
 
         # register_buffer => Tensor which is not a parameter, but should be part of the modules state.
         # Used for tensors that need to be on the same device as the module.
         # persistent=False tells PyTorch to not add the buffer to the state dict (e.g. when we save the model)
         self.register_buffer("position_encoding", position_encoding, persistent=False)
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
         """
 
         Parameters
@@ -181,3 +191,36 @@ class PositionalEncoding(nn.Module):
         """
         x = x + self.position_encoding[:, :x.shape[1], :]  # based on the seq len of x, add position encoding
         return self.dropout(x)
+    
+
+class EmbeddingLayer(nn.Module):
+    def __init__(self, in_features, out_features, model_type="linear", norm_layer=False):
+        """
+        Note: model_type: linear and conv1d with kernel size gives same result, linear is bit faster
+            ref: https://stackoverflow.com/questions/55576314/conv1d-with-kernel-size-1-vs-linear-layer
+        Parameters
+        ----------
+        in_features:
+        out_features
+        model_type
+        """
+        super().__init__()
+        self.model_type = model_type
+        if self.model_type == "linear":
+            self.model = nn.Linear(in_features=in_features, out_features=out_features)
+        elif self.model_type == "conv1d":
+            self.model = nn.Conv1d(  # for conv 1d req shape: batch_size x in_features x seq_len
+                in_features, out_features, 1
+            )
+        else:
+            raise KeyError(f"Model type: {model_type} is not implemented for {self._get_name()} class")
+        self.norm_layer = nn.LayerNorm(out_features) if norm_layer else nn.Identity()
+
+    def forward(self, x):
+        if self.model_type == "linear":
+            x= self.model(x)
+        elif self.model_type == "conv1d":
+            x = self.model(x.permute(0, 2, 1)).permute(  # for conv 1d req shape: batch_size x in_features x seq_len
+                0, 2, 1
+            )  # we change output shape back to shape: batch_size x seq_len x in_features
+        return self.norm_layer(x)
